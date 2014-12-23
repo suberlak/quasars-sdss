7e1fa527f5484bf0bd808a08237da777575823da
Chris Suberlak <suberlak@uw.edu>
2014-12-23 23:22:19 +0100

    awaw

--- qso_CRTS_plot_stats.py
+++ qso_CRTS_plot_stats.py
@@ -57,21 +57,59 @@ assert len(sigma_m) == len(tau_m)
 print '\nOut of ', len(data[:,0]), ' rows we were able to read in ', len(tau_m)
 
 
+
 ##############################
 # LIMIT TO ONLY GOOD POINTS  #
 ##############################
+good_LC = np.loadtxt(dir_in_out[ch] + 'good_err_LC.txt', dtype='str')
+good_LC_cut = np.empty(0, dtype=str)
+for i in range(len(good_LC)):
+    good_LC_cut = np.append(good_LC_cut, good_LC[i][4:-4])
 
-def sel_points(dir_in_out, fname):
-    good_LC = np.loadtxt(dir_in_out + 'good_err_LC.txt', dtype='str')
-    good_LC_mask = np.zeros_like(fname, dtype='bool')
-    for i in range(len(fname)):
-        obj_compared = fname[i][4:]
-        print '\nComparison in progress...', str((float(i) / float(len(fname)) )*100.0)[:5], '%'
-        for name in good_LC :
-            if  obj_compared == name[4:-8] :
-                good_LC_mask[i] = True
-        
-    print 'Out of ', len(fname), 'objects, we use ',  good_LC_mask.sum()
-    return good_LC_mask
+fname_cut = np.empty(0, dtype=str)
+for i in range(len(fname)):
+    fname_cut = np.append(fname_cut, fname[i][:-10])
+
+matched_names = set(good_LC_cut).intersection(fname_cut)
+matched_names = np.array(list(matched_names))
+
+indices = np.zeros_like(fname, dtype=bool)
+for i in range(len(fname)):
+    indices[i] = np.where(fname_cut == matched_names[0])[0][0]
+
     
-good_LC_mask = sel_points(dir_in_out[ch], fname)
+#good_LC_mask = sel_points_qso(dir_in_out[ch], fname)
+
+
+sigma_m = sigma_m[indices]
+tau_m = tau_m[indices]
+
+# recalculate to get sigma hat 
+
+sigma_hat = sigma_m * np.sqrt(tau_m / (2.0 * 365.0))
+
+############################
+#  READ IN STATS FOR LC'S  # 
+############################
+
+stats = np.loadtxt(dir_in_out[ch]+'LC_stats.txt', dtype='str')
+lc_names = stats[:,0]
+
+lc_names_cut = np.empty(0,dtype=str)
+for i in range(len(lc_names)):
+    lc_names_cut = np.append(lc_names_cut, lc_names[i][4:-4])
+    
+inds = np.zeros_like()
+
+mjd_span = stats[:,1]
+mag_rms = stats[:,2]
+mag_mean = stats[:,3]
+err_mean = stats[:,4]
+N_lines = stats[:,5]
+
+##################
+# PLOTTING STATS #
+##################
+
+sigma_neg = [sigma_hat< 0.0]
+sigma_pos = [sigma_hat>0]  
